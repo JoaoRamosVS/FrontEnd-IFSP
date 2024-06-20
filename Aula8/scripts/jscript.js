614c753cada496a4ecbb375e39fd0a8c598b9138
João Vitor <jvsrdez@hotmail.com>
2024-06-19 23:34:02 -0300

    Aula 9 - Introdução ao JavaScript pt. 2 Finalização do projeto da aula  e início do projeto da aula 9

--- Aula8/scripts/jscript.js
+++ Aula8/scripts/jscript.js
@@ -1,15 +1,48 @@
 /* criar os objetos para os elementos que serão manipulados */
 const obj_html = document.querySelector('html')
-
 const obj_sel_cor = document.querySelector('#sel_cor')
 const obj_div_cor = document.querySelector('#div_cor')
-
+const obj_bt_desfocar = document.querySelector('#bt_desfocar')
+const obj_bt_focar = document.querySelector('#bt_focar')
+const obj_img_foco = document.querySelector('#img_foco')
 const obj_cb_fundo_escuro = document.querySelector('#cb_fundo_escuro')
+const array_rb_fonte = document.querySelectorAll('[name="rb_fonte"]')
+const obj_div_area_teste = document.querySelector('#div_area_teste')
+
+const obj_txt_texto_novo = document.querySelector('#txt_texto_novo')
+const obj_bt_troca_texto = document.querySelector('#bt_troca_texto')
+
+for (obj_rb_fonte of array_rb_fonte) {
+    obj_rb_fonte.addEventListener('click', funMudaCorFonte)
+}
+
+function funMudaCorFonte () {
+    for(obj_rb_fonte of array_rb_fonte){
+        if(obj_rb_fonte.checked){
+            obj_div_area_teste.classList.add(obj_rb_fonte.value)
+        }
+        else {
+            obj_div_area_teste.classList.remove(obj_rb_fonte.value)
+        }
+    }   
+}
+
+obj_bt_troca_texto.addEventListener('click', funTrocaTexto)
+
+function funTrocaTexto(){
+    if(obj_txt_texto_novo.value != ''){
+        obj_div_area_teste.innerHTML = obj_txt_texto_novo.value
+    }
+    else{
+        alert('Digite algo no campo!')
+    }
+}
 
 /* Adicionar o evento para chamar a atenção */
-obj_sel_cor.addEventListener('change', funMudaCorFundo);
+obj_sel_cor.addEventListener('change', funMudaCorFundo)
 obj_cb_fundo_escuro.addEventListener('click', funFundoEscuro)
-
+obj_bt_desfocar.addEventListener('click', funDesfocar)
+obj_bt_focar.addEventListener('click', funFocar)
 
 /* Declarar a função para executar as ações */
 function funMudaCorFundo() {
@@ -24,3 +57,13 @@ function funFundoEscuro() {
         obj_html.removeAttribute('class')
     }
 }
+
+function funDesfocar() {
+    obj_img_foco.setAttribute('class', obj_bt_desfocar.value)
+}
+
+function funFocar() {
+    obj_img_foco.setAttribute('class', obj_bt_focar.value)
+}
+
+
---
+++ Aula9/scripts/jscript.js
@@ -0,0 +1,30 @@
+const obj_sel_rede_social = document.querySelector('#sel_rede_social')
+const obj_img_rede_social = document.querySelector('#img_rede_social')
+const obj_p_rede_social = document.querySelector('#p_rede_social')
+
+const array_img_figuras = document.querySelectorAll('.figuras')
+const obj_div_adiciona_figura = document.querySelector("#div_adiciona_figura")
+const obj_img_nova = document.createElement('img')
+const obj_input_novo = document.createElement('input')
+
+obj_sel_rede_social.addEventListener('change', funMudaFigura)
+
+function funMudaFigura() {
+    obj_img_rede_social.setAttribute('src', 'imagens/' + obj_sel_rede_social.value + '.png')
+    obj_img_rede_social.setAttribute('alt', 'Logo ' + obj_sel_rede_social.value)
+    obj_img_rede_social.setAttribute('title', 'Logo ' + obj_sel_rede_social.value)
+
+    obj_p_rede_social.innerHTML = 'Logo ' + obj_sel_rede_social.value
+}
+
+for(obj_img of array_img_figuras) {
+    obj_img.addEventListener('click', function() {funAdicionaFigura(this.src, this.alt)})
+}
+
+function funAdicionaFigura(src, alt) {
+    obj_img_nova.setAttribute('src', src)
+    obj_img_nova.setAttribute('alt', alt)
+    obj_img_nova.setAttribute('title', alt)
+
+    obj_div_adiciona_figura.appendChild(obj_img_nova)
+}
